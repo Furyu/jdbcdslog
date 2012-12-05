6013819ce930cfe5df2dbd39f5b357c43b51e14c
KUOKA Yusuke <ykuoka@gmail.com>
2012-12-05 17:32:58 +0900

    Migrating to sbt with multiple projects

---
+++ project/Build.scala
@@ -0,0 +1,76 @@
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+  val appName = "jdbcdslog-fork"
+  val appVersion = "0.1-SNAPSHOT"
+  val baseName = "jdbcdslog"
+  val appOrganization = "jp.furyu.jdbcdslog_fork"
+
+  lazy val root = Project("root", base = file("."))
+    .aggregate(core, slf4j, fluent, play2)
+
+  lazy val core = Project("core", base = file("core")).settings(
+    organization := appOrganization,
+    version := appVersion,
+    resolvers ++= Seq(
+      "Fluent Maven2 Repository" at "http://fluentd.org/maven2"
+    ),
+    libraryDependencies ++= Seq(
+      "org.fluentd" % "fluent-logger" % "0.2.4",
+      "org.specs2" %% "specs2" % "1.9" % "test",
+      "org.mockito" % "mockito-all" % "1.9.0" % "test",
+      "net.databinder" %% "dispatch-json" % "0.8.5" % "test",
+      "org.slf4j" % "slf4j-api" % "1.5.10" ,
+      "org.slf4j" % "slf4j-log4j12" % "1.5.10" % "test" exclude(
+        "javax.jms", "jms"
+      ) exclude(
+        "javax.mail" , "mail"
+      ) exclude(
+        "com.sun.jdmk", "jmxtools"
+      ) exclude(
+        "com.sun.jmx", "jmxri"
+      ),
+      "log4j" % "log4j" % "1.2.14" % "test" exclude(
+        "javax.jms", "jms"
+      ) exclude(
+        "javax.mail" , "mail"
+      ) exclude(
+        "com.sun.jdmk", "jmxtools"
+      ) exclude(
+        "com.sun.jmx", "jmxri"
+      ),
+      "junit" % "junit" % "4.9" % "test",
+      "hsqldb" % "hsqldb" % "1.8.0.10" % "test"
+    )
+  )
+
+  lazy val slf4j = Project("slf4j", base = file("slf4j")).settings(
+    organization := appOrganization,
+    version := appVersion,
+    resolvers ++= Seq(
+    ),
+    libraryDependencies ++= Seq(
+    )
+  )
+
+  lazy val fluent = Project("fluent", base = file("fluent")).settings(
+    organization := appOrganization,
+    version := appVersion,
+    resolvers ++= Seq( 
+      "Fluent Maven2 Repository" at "http://fluentd.org/maven2"
+    ),
+    libraryDependencies ++= Seq(
+      "org.fluentd" % "fluent-logger" % "0.2.4",
+      "org.specs2" %% "specs2" % "1.9" % "test",
+      "org.mockito" % "mockito-all" % "1.9.0" % "test",
+      "net.databinder" %% "dispatch-json" % "0.8.5" % "test"
+    )
+  )
+  
+
+  lazy val play2 = PlayProject("tracker-sample", path = file("tracker/sample"), mainLang = SCALA).dependsOn(core, slf4j)
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,22 @@
+// sbt 0.11.2や0.11.3向けは https://github.com/typesafehub/sbtscalariform
+// 0.12.0は https://github.com/sbt/sbt-scalariform
+addSbtPlugin("com.typesafe.sbtscalariform" % "sbtscalariform" % "0.5.1")
+
+resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+
+// 0.11.2や0.11.3向けはsbt-idea 1.0.0
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.0.0")
+
+resolvers ++= Seq(
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    Resolver.url("Play", url("http://download.playframework.org/ivy-releases/"))(Resolver.ivyStylePatterns)
+)
+
+addSbtPlugin("play" % "sbt-plugin" % "2.0.1")
+
+resolvers += Resolver.url(
+  "sbt-plugin-releases",
+  new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/")
+)(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.github.retronym" % "sbt-onejar" % "0.8")
