0e15315df20430defc270385fe6556c023951077
KUOKA Yusuke <ykuoka@gmail.com>
2013-01-22 14:29:49 +0900

    Less arrays in formatted request headers, for use from Hive which is not very nice with handling JSON arrays

--- play2/src/main/scala/jp/furyu/play/jdbcdslog/LoggingAction.scala
+++ play2/src/main/scala/jp/furyu/play/jdbcdslog/LoggingAction.scala
@@ -20,7 +20,7 @@ case class AccessContext[A](request: Request[A], additions: Map[String, AnyRef])
         "params" -> queryStringAsJavaMap,
         "timestamp" -> new Date().getTime.asInstanceOf[AnyRef]
       ) ++ Map(
-        "headers" -> request.headers.toMap.map(t => t._1 -> t._2.asJava).asJava,
+        "headers" -> request.headers.toMap.map(t => t._1.toLowerCase -> t._2.headOption.orNull).asJava,
         "languages" -> request.acceptLanguages.map(_.code).asJava
       )
     ).asJava
