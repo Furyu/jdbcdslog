266767219d239a6a53f5a1d39db770d2d40f2f49
KUOKA Yusuke <ykuoka@gmail.com>
2013-02-04 15:51:42 +0900

    Added configuration variable to disable the plugin

--- play2/src/main/scala/jp/furyu/play/jdbcdslog/LoggingAction.scala
+++ play2/src/main/scala/jp/furyu/play/jdbcdslog/LoggingAction.scala
@@ -8,11 +8,38 @@ import org.jdbcdslog.plugin.EventHandlerAPI
 import play.api.libs.iteratee.{Iteratee, Enumerator}
 import play.api.http.{ContentTypes, HeaderNames}
 
-case class ActionProxy[A, B <: Request[_]](
+/**
+ * アクションのプロキシ。アクションの実行をラップして、ログを仕込むような場合に利用します。
+ * @tparam A
+ * @tparam B
+ */
+trait ActionProxy[A, B <: Request[_]] {
+
+  val block: B => Result
+
+  def apply(request: B): Result
+}
+
+/**
+ * テスト中など、ログを無効にしたい場合に利用するActionProxy
+ * アクションログ、レスポンスログの出力や、呼び出し元アクションの情報のjdbcdslogへの送信を行いません。
+ * @param block
+ * @tparam A
+ * @tparam B
+ */
+case class PlainActionProxy[A, B <: Request[_]](
+  block: B => Result) extends ActionProxy[A, B] {
+
+  def apply(request: B): Result = {
+    block(request)
+  }
+}
+
+case class LoggingActionProxy[A, B <: Request[_]](
     logger: AccessLogger = LoggingAction.accessLoggerFromConfiguration,
     eventHandler: FluentEventHandler = LoggingAction.eventHandlerFromConfiguration,
     additions: B => Map[String, AnyRef] = { _: B => Map.empty[String, AnyRef] },
-    block: B => Result) {
+    block: B => Result) extends ActionProxy[A, B] {
   def apply(request: B): Result = {
     val requestContext = AccessContext(request, additions(request))
 
@@ -66,6 +93,21 @@ case class ActionProxy[A, B <: Request[_]](
   }
 }
 
+object ActionProxy {
+  def apply[A, B <: Request[_]](
+             logger: AccessLogger = LoggingAction.accessLoggerFromConfiguration,
+             eventHandler: FluentEventHandler = LoggingAction.eventHandlerFromConfiguration,
+             additions: B => Map[String, AnyRef] = { _: B => Map.empty[String, AnyRef] },
+             block: B => Result)(implicit app: play.api.Application): ActionProxy[A, B] = {
+    val isPluginDisabled = app.configuration.getBoolean("jdbcdslogplugin.disabled").getOrElse(false)
+    if (isPluginDisabled) {
+      new PlainActionProxy[A,B](block)
+    } else {
+      new LoggingActionProxy[A,B](logger, eventHandler, additions, block)
+    }
+  }
+}
+
 /**
  * DB操作などのイベントの文脈情報
  * 例えば、XXというテーブルにinsertしたときのアクセス先アクションはどこだったか、というような文脈をログデータに残すために利用されます。
@@ -104,11 +146,13 @@ class LoggingAction[A](
                         val additions: Request[A] => Map[String, AnyRef] = { _: Request[A] => Map.empty[String, AnyRef] },
                         val parser: BodyParser[A],
                         val logger: AccessLogger,
-                        val eventHandler: FluentEventHandler)
+                        val eventHandler: FluentEventHandler)(implicit app: play.api.Application)
   extends Action[A] {
 
+  val proxy = ActionProxy[A, Request[A]](logger, eventHandler, additions, block)
+
   def apply(request: Request[A]): Result = {
-    ActionProxy[A, Request[A]](logger, eventHandler, additions, block)(request)
+    proxy(request)
   }
 }
 
@@ -133,7 +177,7 @@ object LoggingAction {
                 logger: AccessLogger = accessLoggerFromConfiguration,
                 eventHandler: FluentEventHandler = eventHandlerFromConfiguration
               )(
-                additions: Request[A] => Map[String, AnyRef])(block: Request[A] => Result): LoggingAction[A] =
+                additions: Request[A] => Map[String, AnyRef])(block: Request[A] => Result)(implicit app: play.api.Application): LoggingAction[A] =
     new LoggingAction[A](
       parser = parser,
       logger = logger,
@@ -161,7 +205,7 @@ object LoggingAction {
              eventHandler: FluentEventHandler = eventHandlerFromConfiguration
            )(
              additions: Request[AnyContent] => Map[String, AnyRef]
-           )(block: Request[AnyContent] => Result): LoggingAction[AnyContent] = {
+           )(block: Request[AnyContent] => Result)(implicit app: play.api.Application): LoggingAction[AnyContent] = {
 
     new LoggingAction[AnyContent](
       parser = BodyParsers.parse.anyContent,
--- play2/src/test/scala/jp/furyu/play/jdbcdslog/LoggingActionSpec.scala
+++ play2/src/test/scala/jp/furyu/play/jdbcdslog/LoggingActionSpec.scala
@@ -2,13 +2,17 @@ package jp.furyu.play.jdbcdslog
 
 import org.specs2.mutable._
 import play.api.mvc._
-import play.api.test.FakeRequest
+import play.api.test.{FakeApplication, FakeRequest}
 import play.api.test.Helpers._
 import org.specs2.mock.Mockito
 import jp.furyu.jdbcdslog.fluent.{FluentEventHandler, Context}
 import play.api.libs.json.{JsValue, Json}
+import org.specs2.specification.Scope
+import play.api.libs.iteratee.Enumerator
 
 object LoggingActionSpec extends Specification with Mockito {
+
+  sequential
 
   abstract class Secured[A](block: Request[A] => Result) extends Action[A] {
     def apply(request: Request[A]): Result = {
@@ -25,9 +29,19 @@ object LoggingActionSpec extends Specification with Mockito {
       Secured(BodyParsers.parse.anyContent)(block)
   }
 
+  trait runningFakeApp extends Around {
+    implicit val fakeApplication = new FakeApplication()
+
+    def around[T](t: => T)(implicit evidence$1: (T) => org.specs2.execute.Result): org.specs2.execute.Result = {
+      running(fakeApplication) {
+        t
+      }
+    }
+  }
+
   "LoggingAction" should {
 
-    "send an access log for each request" in {
+    "send an access log for each request" in new runningFakeApp {
 
       val logger = mock[AccessLogger]
 
@@ -57,7 +71,7 @@ object LoggingActionSpec extends Specification with Mockito {
         one(controller.eventHandler).withContext(context)(beOk)
     }
 
-    "be nested inside other action" in {
+    "be nested inside other action" in new runningFakeApp {
 
       val logger = mock[AccessLogger]
 
@@ -93,7 +107,7 @@ object LoggingActionSpec extends Specification with Mockito {
         one(controller.eventHandler).withContext(context)(beOk)
     }
 
-    "nest other action" in {
+    "nest other action" in new runningFakeApp {
 
       val logger = mock[AccessLogger]
 
@@ -129,7 +143,40 @@ object LoggingActionSpec extends Specification with Mockito {
         one(controller.eventHandler).withContext(context)(beOk)
     }
 
-    "log JSON responses" in {
+    "be disabled" in {
+      implicit val app = FakeApplication(additionalConfiguration = Map("jdbcdslogplugin.disabled" -> "true"))
+      running(app) {
+        def ok(body: String): SimpleResult[String] = {
+          SimpleResult(ResponseHeader(OK), Enumerator(body))
+        }
+
+        val ok1 = ok("1")
+        val ok2 = ok("2")
+
+        val logger = mock[AccessLogger]
+        val additions = { req: FakeRequest[AnyContent] => Map("foo" -> "bar") }
+        val eventHandler = mock[FluentEventHandler].defaultReturn(ok2)
+        val block = { req: FakeRequest[AnyContent] => ok1 }
+        val proxy = ActionProxy(logger, eventHandler, additions = additions, block)
+        val request = FakeRequest("GET", "/json?a=b")
+        val context = AccessContext(request, additions(request))
+        val actual = proxy(request)
+
+        status(actual) must be equalTo (OK)
+        contentAsString(actual) must be equalTo ("1")
+
+        def beOk: SimpleResult[String] = beLike[SimpleResult[String]] {
+          case result => {
+            status(result) must be equalTo (200)
+          }
+        }
+
+        there was no(logger).log(any[Context])
+        there was no(eventHandler).withContext(context)(any[SimpleResult[String]])
+      }
+    }
+
+    "log JSON responses" in new runningFakeApp {
       val logger = mock[AccessLogger]
 
       val request = FakeRequest("GET", "/json?a=b")
@@ -231,7 +278,7 @@ object LoggingActionSpec extends Specification with Mockito {
       val controller = new Controller {
         val eventHandler = mock[FluentEventHandler].defaultReturn(Ok("stubbed"))
         val response = Ok(Json.toJson(Map("ok" -> 1)))
-        val proxy = ActionProxy[AnyContent, AuthorizedRequest[AnyContent]](logger, eventHandler, (req: AuthorizedRequest[AnyContent]) => additions, { request: AuthorizedRequest[AnyContent] => response })
+        val proxy = LoggingActionProxy[AnyContent, AuthorizedRequest[AnyContent]](logger, eventHandler, (req: AuthorizedRequest[AnyContent]) => additions, { request: AuthorizedRequest[AnyContent] => response })
         def foo = Authorized(BodyParsers.parse.anyContent) { request =>
           proxy(request)
         }
